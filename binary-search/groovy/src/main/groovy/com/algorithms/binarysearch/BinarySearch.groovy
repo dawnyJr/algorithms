f4fb8eed63c3a38bcd4a47c615c28076d67fbf98
Sabiq Ihab <sabiq.ihab@hotmail.fr>
2017-10-29 14:30:59 +0000

    add groovydoc

--- binary-search/groovy/src/main/groovy/com/algorithms/binarysearch/BinarySearch.groovy
+++ binary-search/groovy/src/main/groovy/com/algorithms/binarysearch/BinarySearch.groovy
@@ -1,10 +1,20 @@
 package com.algorithms.binarysearch
 
 /**
+ * Class containing two methods performing a binary search on a sorted array, one using the recursive approach and the other using the iterative approach.
+ *
  * Created by Sabiq Ihab on 10/28/17.
  */
 class BinarySearch {
-
+/**
+ * find a value in a sorted array.
+ *
+ * @param value the value being search for in the array.
+ * @param sortedArray array where to look for the value, must be sorted.
+ * @param start optional argument to specify the index on the left of the array, default 0.
+ * @param end optional argument to specify the index on the right of the array, default size of the array minis 1.
+ * @return if the value is found its position is returned else it returns -1.
+ */
     def recursiveSearch(int value, int[] sortedArray, int start = 0, int end = sortedArray.size() - 1) {
         int m
         if (start > end) {
@@ -21,7 +31,13 @@ class BinarySearch {
         }
 
     }
-
+/**
+ * find a value in a sorted array.
+ *
+ * @param value the value being search for in the array.
+ * @param sortedArray array where to look for the value, must be sorted.
+ * @return if the value is found its position is returned else it returns -1.
+ */
     def search(int value, int[] sortedArray) {
         def start = 0
         def end = sortedArray.size()
