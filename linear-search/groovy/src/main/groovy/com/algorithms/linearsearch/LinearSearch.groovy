8c252b243b096e54048bda261c255a8bfe6993b0
Sabiq Ihab <sabiq.ihab@hotmail.fr>
2017-11-15 23:30:40 +0000

    add linear search implementation

--- linear-search/groovy/src/main/groovy/com/algorithms/linearsearch/LinearSearch.groovy
+++ linear-search/groovy/src/main/groovy/com/algorithms/linearsearch/LinearSearch.groovy
@@ -1,7 +1,15 @@
 package com.algorithms.linearsearch
-
 /**
  * Created by Sabiq Ihab on 11/15/17.
  */
 class LinearSearch {
+
+    def search(int v, int[] t) {
+
+        for (int i = 0; i < t.size(); i++) {
+            if (t[i] == v)
+                return i
+        }
+        return -1
+    }
 }
