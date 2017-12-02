0d812711b55bc367db4f808fa3e46e14bb6d06b7
Mossaab Stiri <mossaab.stiri@gmail.com>
2017-12-02 00:34:12 +0000

    Renaming variable in test class

--- linear-search/python/test_linear_search.py
+++ linear-search/python/test_linear_search.py
@@ -5,19 +5,19 @@ import linear_search as search
 class TestLinearSearch(unittest.TestCase):
 
     def setUp(self):
-        list = [3, 5, 12, -5, 0, 45, 78]
+        self.my_list = [3, 5, 12, -5, 0, 45, 78]
 
     def test_search_item(self):
-        self.assertEqual(2, search.linear_search(list, 12))
+        self.assertEqual(2, search.linear_search(self.my_list, 12))
 
     def test_search_first(self):
-        self.assertEqual(0, search.linear_search(list, 3))
+        self.assertEqual(0, search.linear_search(self.my_list, 3))
 
     def test_search_last(self):
-        self.assertEqual(6, search.linear_search(list, 78))
+        self.assertEqual(6, search.linear_search(self.my_list, 78))
 
     def test_search_inexisting(self):
-        self.assertEqual(-1, search.linear_search(list, 4))
+        self.assertEqual(-1, search.linear_search(self.my_list, 4))
 
 if __name__ == '__main__':
     unittest.main()
